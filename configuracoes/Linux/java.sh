af849a42140db7c447040c20b9aea05adeb27a0d
Paulo Jerônimo <paulojeronimo@gmail.com>
2015-02-07 11:13:29 -0200

    Ajuste do JAVA_HOME para o Fedora

--- configuracoes/Linux/java.sh
+++ configuracoes/Linux/java.sh
@@ -1,6 +1,6 @@
 distro=`distro`
 case $distro in
-    CentOS|Fedora) export JAVA_HOME=/usr/java/jdk1.8.0_31;;
+    CentOS|Fedora) export JAVA_HOME=/usr/java/latest;;
     Ubuntu) export JAVA_HOME=/usr/lib/jvm/java-8-oracle;;
     *) echo "Configure JAVA_HOME para a distribuição \"$distro\"!"
 esac
