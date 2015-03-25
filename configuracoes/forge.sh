7e4e0d71bb274670e4e9fd5c40d150261381c471
U-STN\fabio.s.barbosa <fabio.s.barbosa@BRJ4050ZLH.stn.intra>
2015-03-25 17:30:39 -0300

    Atualização da versão do Forge

--- configuracoes/forge.sh
+++ configuracoes/forge.sh
@@ -1,9 +1,9 @@
 FORGE_LINK=forge
 FORGE_PACKAGE=forge-distribution
-FORGE_VER=2.15.1.Final
+FORGE_VER=2.15.2.Final
 FORGE_DIR=$FORGE_PACKAGE-$FORGE_VER
 FORGE_INSTALADOR=$FORGE_DIR-offline.zip
-FORGE_INSTALADOR_URL_COMPLETA=https://repository.jboss.org/nexus/service/local/artifact/maven/redirect\?r\=releases\&g\=org.jboss.forge\&a\=forge-distribution\&v\=LATEST\&e\=zip\&c\=offline
+FORGE_INSTALADOR_URL_COMPLETA="https://repository.jboss.org/nexus/service/local/artifact/maven/redirect?r=releases&g=org.jboss.forge&a=forge-distribution&v=$FORGE_VER&e=zip&c=offline"
 
 export FORGE_HOME=$FERRAMENTAS_DIR/forge
 export PATH=$FORGE_HOME/bin:$PATH
