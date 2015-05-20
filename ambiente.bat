27b3d7b2b4742cb2f3311af57d3a9d5770627118
Paulo Jerônimo <paulojeronimo@gmail.com>
2015-05-20 15:47:08 -0300

    Ajustes para funcionamento do keycloak e do asciidoctor

--- ambiente.bat
+++ ambiente.bat
@@ -4,9 +4,18 @@ set AMBIENTE_HOME=%USERPROFILE%\javaee-ambiente
 
 set FERRAMENTAS_HOME=%AMBIENTE_HOME%\ferramentas
 
-set JBOSS_HOME=%FERRAMENTAS_HOME%\jboss-eap-6.4
+rem #### Configuração do JBoss - Início ####
+rem JBoss EAP 6.4
+rem set JBOSS_HOME=%FERRAMENTAS_HOME%\jboss-eap-6.4
+
+rem JBoss BPMS
 rem set JBOSS_HOME=%FERRAMENTAS_HOME%\jboss-bpms-6.0.3.GA-AD\jboss-eap-6.1
+
+rem Keycloak
+set JBOSS_HOME=%FERRAMENTAS_HOME%\keycloak-1.2.0.Final
+
 set PATH=%JBOSS_HOME%\bin;%PATH%
+rem #### Configuração do JBoss - Fim ####
 
 set ECLIPSE_HOME=%FERRAMENTAS_HOME%\eclipse.Cygwin
 set PATH=%ECLIPSE_HOME%;%PATH%
@@ -25,6 +34,9 @@ set PATH=%ANT_HOME%\bin;%PATH%
 
 set JAVA_HOME=C:\Program Files\Java\jdk1.7.0_75
 set PATH=%JAVA_HOME%\bin;%PATH%
+
+rem PATH p/ localização de binários instalados via gem (ex.: asciidoctor)
+set PATH=%USERPROFILE%\bin;%PATH%
 
 set CYGWIN_HOME=C:\cygwin64
 set PATH=%CYGWIN_HOME%\bin;%PATH%
