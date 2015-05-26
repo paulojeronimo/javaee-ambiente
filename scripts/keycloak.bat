1f370b29254fd53071890a81ba09afd1e81a8454
Paulo Jerônimo <paulojeronimo@gmail.com>
2015-05-26 14:30:02 -0300

    Bug fix na inicialização do keycloak

--- scripts/keycloak.bat
+++ scripts/keycloak.bat
@@ -4,4 +4,4 @@ call ..\ambiente.bat
 
 set JBOSS_HOME=%KEYCLOAK_HOME%
 
-call %JBOSS_HOME%\bin\standalone.bat -bjboss.socket.binding.port-offset=%KEYCLOAK_OFFSET%
+call %JBOSS_HOME%\bin\standalone.bat -Djboss.socket.binding.port-offset=%KEYCLOAK_OFFSET%
