beafa055833de517af63cf7329522ec36c6247cc
Paulo Jerônimo <paulojeronimo@gmail.com>
2015-06-06 11:36:41 -0300

    Adiciona configurações para uma versão antiga do keycloak e simplifica o carregamento do ambiente

--- configuracoes/keycloak.sh
+++ configuracoes/keycloak.sh
@@ -1,13 +1,24 @@
+KEYCLOAK_VER=${KEYCLOAK_VER:-1.2.0.Final}
 KEYCLOAK_LINK=keycloak
-KEYCLOAK_VER=1.2.0.Final
-KEYCLOAK_DIR=keycloak-$KEYCLOAK_VER
-KEYCLOAK_INSTALADOR=$KEYCLOAK_DIR.tar.gz
-KEYCLOAK_INSTALADOR_URL_COMPLETA=http://nbtelecom.dl.sourceforge.net/project/keycloak/$KEYCLOAK_VER/$KEYCLOAK_INSTALADOR
+
+case $KEYCLOAK_VER in
+    1.1.0.Final)
+        KEYCLOAK_DIR=keycloak-appliance-dist-all-$KEYCLOAK_VER
+        KEYCLOAK_INSTALADOR=$KEYCLOAK_DIR.zip
+        KEYCLOAK_HOME=$FERRAMENTAS_DIR/keycloak/keycloak
+        ;;
+    1.2.0.Final)
+        KEYCLOAK_DIR=keycloak-$KEYCLOAK_VER
+        KEYCLOAK_INSTALADOR=$KEYCLOAK_DIR.tar.gz
+        KEYCLOAK_HOME=$FERRAMENTAS_DIR/keycloak
+        ;; 
+esac
+KEYCLOAK_INSTALADOR_URL_COMPLETA=http://ufpr.dl.sourceforge.net/project/keycloak/$KEYCLOAK_VER/$KEYCLOAK_INSTALADOR
 
 case $PLATAFORMA in
     Cygwin) export KEYCLOAK_HOME=`cygpath "$KEYCLOAK_HOME"`;;
     *) 
-        export KEYCLOAK_HOME=$FERRAMENTAS_DIR/keycloak
+        export KEYCLOAK_HOME
         export PATH=$KEYCLOAK_HOME/bin:$PATH
         ;;
 esac
