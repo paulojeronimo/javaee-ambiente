320e8c6daf1784bb492407c075a1c5161311ca9e
Paulo Jerônimo <paulojeronimo@gmail.com>
2015-06-09 17:15:29 -0300

    Adição de novas funcionalidades ao script do keycloak

--- funcoes/keycloak.sh
+++ funcoes/keycloak.sh
@@ -1,10 +1,19 @@
 keycloak() {
     local op=$1
     local offset=${KEYCLOAK_OFFSET:-0}
-
     case $op in
         install)
-            instalar keycloak "$@"
+            if [ "$2" = "examples" ]
+            then
+                if [ "$KEYCLOAK_EXAMPLES_URL" ]
+                then
+                    wget -c -O "$INSTALADORES_DIR"/"${KEYCLOAK_EXAMPLES_URL##*/}" $KEYCLOAK_EXAMPLES_URL
+                    rm -rf "$PROJETOS_DIR"/${KEYCLOAK_EXAMPLES_DIR/}
+                    unzip -d "$PROJETOS_DIR" "$INSTALADORES_DIR"/${KEYCLOAK_EXAMPLES_URL##*/}
+                fi
+            else
+                instalar keycloak "$@"
+            fi
             ;;
         change)
             pushd "$FERRAMENTAS_HOME" &> /dev/null
