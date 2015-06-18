1664cb7eb53a0050aac034893310cbe9c1bcde38
Paulo Jerônimo <paulojeronimo@gmail.com>
2015-06-18 08:33:40 -0300

    No ambiente Cygwin, faz a inicialização do bpms ser realizada pelo script bpms.bat

--- funcoes/bpms.sh
+++ funcoes/bpms.sh
@@ -6,9 +6,16 @@ bpms() {
     case $op in
         start)
             # inicia o BPMS em seu diretório HOME pois serão criados alguns diretórios nele (.index .niogit .scecurity repositories)
-            cd "$BPMS_HOME"
-            JBOSS_HOME=$BPMS_EAP_HOME "$BPMS_EAP_HOME"/bin/standalone.sh
-            cd - &> /dev/null
+            if [ "$PLATAFORMA" = "Cygwin" ]
+            then
+                scripts
+                bpms.bat
+                cd - &> /dev/null
+            else
+                cd "$BPMS_HOME"
+                JBOSS_HOME=$BPMS_EAP_HOME "$BPMS_EAP_HOME"/bin/standalone.sh
+                cd - &> /dev/null
+            fi
             ;;
         stop)
             JBOSS_HOME=$BPMS_EAP_HOME "$BPMS_EAP_HOME"/bin/jboss-cli.sh -c :shutdown
