00e707e20b1e9462a14818b77c85e933f3e512b4
Paulo Jerônimo <paulojeronimo@gmail.com>
2015-06-25 17:48:31 -0300

    Adição do sqldeveloper

---
+++ configuracoes/sqldeveloper.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export SQLDEVELOPER_HOME=$FERRAMENTAS_DIR/sqldeveloper
+
+# vim set ts=4, sw=4, expandtab:
---
+++ funcoes/Cygwin/sqldeveloper.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sqldeveloper() {
+    "$SQLDEVELOPER_HOME"/sqldeveloper.exe "$@"
+}
+
+# vim: set ts=4 sw=4 expandtab:
