04b63cdc1ab6c47a2088aa65219a6d6fccd4a69a
Paulo Jerônimo <paulojeronimo@gmail.com>
2015-09-21 15:37:24 -0300

    Atualização do JBoss Forge para a versão 2.19.2.Final

--- configuracoes/forge.sh
+++ configuracoes/forge.sh
@@ -12,7 +12,7 @@ case $PLATAFORMA in
         ;;
 esac
 
-FORGE_VER=${FORGE_VER:-2.19.0.Final}
+FORGE_VER=${FORGE_VER:-2.19.2.Final}
 FORGE_DIR=forge-distribution-$FORGE_VER
 FORGE_INSTALADOR=$FORGE_DIR-offline.zip
 FORGE_INSTALADOR_URL_COMPLETA="https://repository.jboss.org/nexus/service/local/artifact/maven/redirect?r=releases&g=org.jboss.forge&a=forge-distribution&v=$FORGE_VER&e=zip&c=offline"
