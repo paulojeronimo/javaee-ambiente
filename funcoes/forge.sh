7055a69bc6725754f105eff3bd1c0d5310b133a2
Paulo Jerônimo <paulojeronimo@gmail.com>
2015-09-21 15:40:36 -0300

    Inclusão da instalação do addon para o Arquillian

--- funcoes/forge.sh
+++ funcoes/forge.sh
@@ -8,6 +8,9 @@ forge_instalar_addons() {
 
     echo "Instalando o addon para o AngularJS ..."
     forge -b -i org.jboss.forge.addon:angularjs
+
+    echo "Instalando o addon para o Arquillian ..."
+    forge -b -i org.arquillian.forge:arquillian-addon
 }
 
 # vim: set ts=4 sw=4 expandtab:
